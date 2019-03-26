68af2cbf36934adb8aced97fe5743923a6f29da2
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2019-03-26 15:09:33 +0100

    Dependencies angepasst u.a. kleinere Korrekturen

--- src/main/java/HelloWorldApp.java
+++ src/main/java/HelloWorldApp.java
@@ -1,7 +1,7 @@
 public class HelloWorldApp {
 
 	public static void main(String[] args) {
-		System.out.println("Hello " + (args.length > 0 ? args[0] : "Welt") + "!");
+		System.out.println("Hallo " + (args.length > 0 ? args[0] : "Welt") + "!");
 	}
 
 }
